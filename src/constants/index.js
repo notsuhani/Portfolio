3ae12658046234c1b2364ddedb40b528a531eb05
Suhani Mishra <suhani1657a@gmail.com>
2025-08-26 09:36:29 +0530

    Initial commit of my portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,280 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "E-commerce Platform",
+    description:
+      "Facilitates purchases from international websites like Amazon and eBay, allowing customers to shop from these sites and have products delivered domestically.",
+    subDescription: [
+      "Built a scalable application with ASP.NET Core MVC, integrating global platforms like Amazon for domestic delivery.",
+      "Implemented secure authentication and database management using ASP.NET Core Identity and Entity Framework Core.",
+      "Designed a responsive frontend with Tailwind CSS, enhancing user experience.",
+      "Added payment systems, localization, and product filtering for functionality improvements.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/accessories.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "MongoDB",
+        path: "/assets/logos/MongoDB.svg",
+      },
+      {
+        id: 2,
+        name: "NODE.JS",
+        path: "/assets/logos/node.svg",
+      },
+      {
+        id: 3,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 4,
+        name: "ExpressJS",
+        path: "/assets/logos/Express.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Authentication & Authorization System",
+    description:
+      "A secure authentication and authorization system using Auth0 for seamless user management.",
+    subDescription: [
+      "Integrated Auth0 for authentication, supporting OAuth, JWT, and multi-factor authentication.",
+      "Implemented role-based access control (RBAC) for fine-grained user permissions.",
+      "Developed a React-based frontend with Tailwind CSS for a sleek user experience.",
+      "Connected to a secure SQLite database for user data storage.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/auth-system.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/MongoDB.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Sort-Visualizer",
+    description:
+      "An interactive, web-based sorting visualizer that brings algorithms to life by providing dynamic animations for popular sorting techniques such as Bubble Sort, Insertion Sort, Merge Sort, and Quick Sort..",
+    subDescription: [
+      "Designed with a clean and responsive interface, this project enables users to:",
+      "Visualize each step of the sorting process through smooth animations.",
+      "Track real-time updates of time and space complexities, helping learners compare algorithm efficiency.",
+      "Adjust input size and speed to observe algorithm performance under different conditions.",
+      "Gain a clear, practical understanding of sorting techniques, making it an ideal educational tool for students, coding enthusiasts, and interview preparation.",
+    ],
+    href: "https://sort-visualizer-beta-green.vercel.app/",
+    logo: "",
+    image: "/assets/projects/sort.png",
+    tags: [
+      {
+        id: 1,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 3,
+        name: "Javascript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Blog Application",
+    description:
+      "Developed a full-stack blog application using the MERN stack, implementing key features to enhance user experience and content management..",
+    subDescription: [
+      "Implemented user authentication and authorization using JWT and bcrypt for secure login and role-based access.",
+      "Developed full CRUD operations for blog posts with Express.js APIs and MongoDB for data storage.",
+      "Designed a responsive UI with search and pagination using React and Tailwind CSS for a seamless user experience.",
+      "Built a comments and likes feature, enabling real-time user interaction on posts.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/game-engine.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "NodeJS",
+        path: "/assets/logos/node.svg",
+      },
+      {
+        id: 3,
+        name: "Javascript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 4,
+        name: "CSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  // {
+  //   id: 5,
+  //   title: "WordPress Custom Theme",
+  //   description:
+  //     "A fully customizable WordPress theme optimized for performance and SEO.",
+  //   subDescription: [
+  //     "Developed a responsive WordPress theme using HTML5, CSS3, and JavaScript.",
+  //     "Integrated Tailwind CSS for modern styling and UI enhancements.",
+  //     "Optimized SEO and page speed using Vite.js for fast builds.",
+  //     "Implemented custom widgets and plugin compatibility for extended functionality.",
+  //   ],
+  //   href: "",
+  //   logo: "",
+  //   image: "/assets/projects/wordpress-theme.jpg",
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "WordPress",
+  //       path: "/assets/logos/wordpress.svg",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "HTML5",
+  //       path: "/assets/logos/html5.svg",
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "CSS3",
+  //       path: "/assets/logos/css3.svg",
+  //     },
+  //     {
+  //       id: 4,
+  //       name: "Vite.js",
+  //       path: "/assets/logos/vitejs.svg",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 6,
+  //   title: "Online Learning Platform",
+  //   description:
+  //     "A web application that allows users to enroll in courses, watch video lectures, and take quizzes.",
+  //   subDescription: [
+  //     "Built using Blazor WebAssembly for a seamless SPA experience.",
+  //     "Implemented video streaming with Azure Media Services.",
+  //     "Added a quiz system with dynamic question generation and real-time grading.",
+  //     "Integrated Stripe API for secure payment processing.",
+  //   ],
+  //   href: "",
+  //   logo: "",
+  //   image: "/assets/projects/elearning.jpg",
+  //   tags: [
+  //     {
+  //       id: 1,
+  //       name: "Blazor",
+  //       path: "/assets/logos/blazor.svg",
+  //     },
+  //     {
+  //       id: 2,
+  //       name: "Azure",
+  //       path: "/assets/logos/azure.svg",
+  //     },
+  //     {
+  //       id: 3,
+  //       name: "Stripe",
+  //       path: "/assets/logos/stripe.svg",
+  //     },
+  //     {
+  //       id: 4,
+  //       name: "TailwindCSS",
+  //       path: "/assets/logos/tailwindcss.svg",
+  //     },
+  //   ],
+  // },
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "https://github.com/notsuhani",
+    icon: "/assets/socials/github-mark-white.png",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/suhani-mishra12/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  // {
+  //   name: "Instagram",
+  //   href: "https://www.instagram.com/ali.sanatidev/reels/",
+  //   icon: "/assets/socials/instagram.svg",
+  // },
+];
+
+export const experiences = [
+  {
+    title: "C++ Intern",
+    job: "Centre for Railway & Information (CRIS)",
+    date: "June 2024 - July 2024",
+    contents: [
+      "➜ Assisted in developing and optimizing modules of an internal railway software used for scheduling and operational data handling.",
+      "➜ Worked on an internal C++-based CLI application called “RailwayAuth – Admin & User Validation System”.",
+      "➜Built a menu-driven interface simulating real-world user data entry in railway systems.",
+      "➜ Gained experience in C++, OOPs, STL, File Handling and memory management",
+      "➜ Utilized Git for version control, managing code changes and collaborating effectively with team members.",
+    ],
+  },
+  {
+    title: "SQL Intern",
+    job: "Celebal Technologies",
+    date: "June 2025 - Aug 2025",
+    contents: [
+      "➜ Worked on real-time SQL projects involving data extraction, analysis, and reporting.",
+      "➜ Wrote optimized queries using joins, subqueries, group by, and window functions.",
+      "➜ Analyzed and managed datasets from the AdventureWorks2022 database.",
+      "➜ Gained hands-on experience in database design, normalization, and query optimization.",
+      "➜ Improved problem-solving and data-handling skills through regular assignments and reviews.",
+    ],
+  },
+  {
+    // title: "Trainings & Ideathon",
+    job: "Various trainings & Ideathon",
+    date: "Trainings & Ideathon",
+    contents: [
+      "➜ Completed a Machine Learning Training Program by IIT Delhi, gaining hands-on experience with ML algorithms and model building.",
+      "➜ Took part in the Cisco Ideathon process, working on innovative solutions in the domain of AI and cybersecurity.",
+      "➜ Actively engaged in collaborative team activities, improving communication and teamwork skills."
+    ],
+  },
+];
